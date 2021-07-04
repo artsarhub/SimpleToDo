cbb043a41f9f7651f587cb4044e139bed271c40a
SaranaArtem <a.sarana@assistagro.com>
2021-07-04 15:45:42 +0300

    Initial Commit

---
+++ SimpleToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleToDo
+//
+//  Created by Артём on 04.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
